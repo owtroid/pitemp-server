f1de5cec5525053712a881a23628b872aa1355bf
Torgny Johansson <torgny.johansson@fortnox.se>
2015-12-13 22:35:06 +0100

    Add sensor impl class

---
+++ src/main/java/com/owtroid/pits/sensors/TempSensor.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2015 Pivotal Software, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.owtroid.pits.sensors;
+
+/**
+ *
+ * @author torgny
+ */
+public class TempSensor implements Sensor<Double> {
+
+    private Double value;
+    private String id, description, name;
+    
+    public TempSensor(String id, String name, String description, Double value) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
+    
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Double getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
